ab8a1c7cdde2bb5fbab8d84c5ddecc01e1d59e82
Lukasz Zephyr <s-zephyr@o2.pl>
2025-11-07 23:26:42 +0100

    add tile map

--- debris/debris.gd
+++ debris/debris.gd
@@ -2,12 +2,12 @@ class_name Debris
 extends RigidBody2D
 
 const MAX_SPEED = 1_200
-const IMPULSE_STR = 20
+const IMPULSE_STR = 2_000
 const BOOM_IMPULSE_STR = 10_000
 
 
 func push_from_player(player:Node2D, c:KinematicCollision2D):
-	apply_impulse(-1 *  IMPULSE_STR * c.get_normal())
+	apply_force(-1 *  IMPULSE_STR * c.get_normal())
 
 func push_from_boom(boom_pos:Vector2):
 	var n = (global_position - boom_pos).normalized()
