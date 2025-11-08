a7043b26207d56394d3cca552616cd0765f7521e
Lukasz Zephyr <s-zephyr@o2.pl>
2025-11-08 03:26:12 +0100

    level 3 + cheats

---
+++ GLOBAL.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _ready() -> void:
+	Console.font_size = 45
+	Console.pause_enabled = true
+	Console.add_command("lv", set_level, ["name"])
+
+func set_level(lv_name: String):
+	print("changing scene to ", lv_name)
+	get_tree().change_scene_to_file("res://levels/level"+lv_name+".tscn")
